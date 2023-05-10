5afe2c2468ba4c9fea96ec0fe93d1462eea19ef6
Vincent Noël <vincent.noel@curie.fr>
2023-05-10 16:02:58 +0200

    Back to using paint_by_density for substrates

--- sample_projects_intracellular/boolean/tutorial/main.cpp
+++ sample_projects_intracellular/boolean/tutorial/main.cpp
@@ -154,7 +154,7 @@ int main( int argc, char* argv[] )
 	// for simplicity, set a pathology coloring function 
 	
 	std::vector<std::string> (*cell_coloring_function)(Cell*) = paint_by_number_cell_coloring; 
-	std::vector<std::string> (*substrate_coloring_function)(double, double, double) = NULL; 
+	std::vector<std::string> (*substrate_coloring_function)(double, double, double) = paint_by_density_percentage; 
 	
 	sprintf( filename , "%s/initial.svg" , PhysiCell_settings.folder.c_str() ); 
 	SVG_plot( filename , microenvironment, 0.0 , PhysiCell_globals.current_time, cell_coloring_function, substrate_coloring_function);
