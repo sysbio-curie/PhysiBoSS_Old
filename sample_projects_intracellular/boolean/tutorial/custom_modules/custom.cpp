c616dba1210198d919a8d83bcb29813b303bd8b3
Vincent Noël <vincent.noel@curie.fr>
2024-02-05 13:36:03 -0300

    Added treatment function in the custom_module

--- sample_projects_intracellular/boolean/tutorial/custom_modules/custom.cpp
+++ sample_projects_intracellular/boolean/tutorial/custom_modules/custom.cpp
@@ -206,3 +206,36 @@ void custom_function( Cell* pCell, Phenotype& phenotype , double dt )
 
 void contact_function( Cell* pMe, Phenotype& phenoMe , Cell* pOther, Phenotype& phenoOther , double dt )
 { return; } 
+
+void treatment_function () 
+{
+	if (PhysiCell::parameters.bools.find_index("treatment") != -1) 
+	{
+		int treatment_substrate_index = BioFVM::microenvironment.find_density_index(PhysiCell::parameters.strings("treatment_substrate"));
+
+		if (PhysiCell::parameters.bools("treatment")){
+		
+			if (
+				(((int)PhysiCell::PhysiCell_globals.current_time) % PhysiCell::parameters.ints("treatment_period")) == 0 
+				&& !BioFVM::microenvironment.get_substrate_dirichlet_activation(treatment_substrate_index)
+			)
+			{
+				std::cout << PhysiCell::parameters.strings("treatment_substrate") << " activation at t=" << PhysiCell::PhysiCell_globals.current_time << std::endl;
+				BioFVM::microenvironment.set_substrate_dirichlet_activation(treatment_substrate_index, true);	
+			}
+
+			if (
+				(((int)PhysiCell::PhysiCell_globals.current_time) % PhysiCell::parameters.ints("treatment_period")) == PhysiCell::parameters.ints("treatment_duration") 
+				&& BioFVM::microenvironment.get_substrate_dirichlet_activation(treatment_substrate_index)
+			)
+			{
+				std::cout << PhysiCell::parameters.strings("treatment_substrate") << " inactivation at t=" << PhysiCell::PhysiCell_globals.current_time << std::endl;
+				BioFVM::microenvironment.set_substrate_dirichlet_activation(treatment_substrate_index, false);	
+			}
+			
+		} else if ( BioFVM::microenvironment.get_substrate_dirichlet_activation(treatment_substrate_index) ){
+			std::cout << PhysiCell::parameters.strings("treatment_substrate") << " inactivation (NO TREATMENT) at t=" << PhysiCell::PhysiCell_globals.current_time << std::endl;
+			BioFVM::microenvironment.set_substrate_dirichlet_activation(treatment_substrate_index, false);	
+		}
+	}
+}
